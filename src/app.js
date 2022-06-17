afea1820ae4a2afb97bdc026ee09f88fa845b70b
olenadidenko <77194573+olenadidenko@users.noreply.github.com>
2022-06-17 22:45:05 +0300

    current day, date,hours

--- src/app.js
+++ src/app.js
@@ -1,17 +1,58 @@
+function formatData(timestemp){
+    let now= new Date(timestemp);
+    let date = now.getDate()
+    let hours = now.getHours();
+        if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = now.getMinutes();
+        if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"];
+    let day = days[now.getDay()];
+
+    let mounths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"];
+    let mounth = mounths[now.getMonth()];
+    return `${day}, ${date} ${mounth}, ${hours}:${minutes}`
+}
+
 function getTemp(response){
     console.log(response)
     let currentTemp = document.querySelector("#degrees-temp");
     let currentCity = document.querySelector("#seached-city");
-    let currenHumidity = document.querySelector(".humidity");
-    let currenDescription = document.querySelector(".weather");
-    let currenWind = document.querySelector(".wind");
-    let currenPressure = document.querySelector(".pressure");
+    let currentHumidity = document.querySelector(".humidity");
+    let currentDescription = document.querySelector(".weather");
+    let currentWind = document.querySelector(".wind");
+    let currentPressure = document.querySelector(".pressure");
+    let currentDate = document.querySelector(".todayIs");
     currentTemp.innerHTML = Math.round (response.data.main.temp);
     currentCity.innerHTML = response.data.name;
-    currenHumidity.innerHTML = response.data.main.humidity;
-    currenDescription.innerHTML = response.data.weather[0].description;
-    currenWind.innerHTML = Math.round(response.data.wind.speed);
-    currenPressure.innerHTML = response.data.main.pressure;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentDescription.innerHTML = response.data.weather[0].description;
+    currentWind.innerHTML = Math.round(response.data.wind.speed);
+    currentPressure.innerHTML = response.data.main.pressure;
+    currentDate.innerHTML = formatData(response.data.dt * 1000);
 }
 let apiKey = "28380c9029ac812a2a683ccc768f6493";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${apiKey}&units=metric`;
